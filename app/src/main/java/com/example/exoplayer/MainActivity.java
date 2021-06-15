51a44dea985e9355b2a93ea95ba7a7e7e314275d
Alexandre Rocha Coutinho <alex.rochacoutinho@gmail.com>
2021-06-16 00:34:53 +0200

    First ExoPlayer implementation, with working video.

--- app/src/main/java/com/example/exoplayer/MainActivity.java
+++ app/src/main/java/com/example/exoplayer/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.exoplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.exoplayer.databinding.ActivityMainBinding;
+import com.google.android.exoplayer2.MediaItem;
+import com.google.android.exoplayer2.SimpleExoPlayer;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    private Uri videoUri = Uri.parse("http://qthttp.apple.com.edgesuite.net/1010qwoeiuryfg/sl.m3u8");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        setUpExoPlayer();
+    }
+
+    private void setUpExoPlayer() {
+        SimpleExoPlayer player = new SimpleExoPlayer.Builder(getApplicationContext()).build();
+        binding.playerView.setPlayer(player);
+
+        // Build the media item.
+        MediaItem mediaItem = MediaItem.fromUri(videoUri);
+        // Set the media item to be played.
+        player.setMediaItem(mediaItem);
+        // Prepare the player.
+        player.prepare();
+        // Start the playback.
+        player.play();
     }
 }
